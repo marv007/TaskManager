12447b9f3313531fe38230265764cc5cc1b23c27
Marvin Torres <marvintorres2013@gmail.com>
2022-03-02 20:35:22 -0600

    Agrega seeders para roles y usuario admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        
+        $user->assignRole('admin');
+       
+    }
+}
